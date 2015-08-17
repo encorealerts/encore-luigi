46dafed94d8735f7504a4f3b000342b939b0bcb5
felipeclopes <felipelopes10@gmail.com>
2015-08-16 22:42:56 -0700

    added: summary weight in actor classification

--- actor_classification.py
+++ actor_classification.py
@@ -20,10 +20,15 @@ import csv
 
 from twitter import *
 
+import nltk
+from nltk import word_tokenize
+
 from sklearn.cross_validation import KFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.externals import joblib
+
+from sklearn.feature_extraction.text import CountVectorizer
 
 from dateutil import relativedelta
 from dateutil import parser
@@ -54,8 +59,9 @@ class DownloadTrainerData(S3ToLocalTask):
 
 class EnrichTrainerData(luigi.Task):
   date = luigi.DateParameter()
-  input_prefix = luigi.Parameter(default='data/actor_classification/raw/twitter-actor.csv')
-  output_prefix = luigi.Parameter(default='data/actor_classification/csv/enriched-twitter-actor.csv')
+  
+  input_prefix = 'data/actor_classification/raw/twitter-actor.csv'
+  output_prefix = 'data/actor_classification/csv/enriched-twitter-actor.csv'
 
   token = luigi.Parameter(default='22911906-GR7LBJ2oil3cc27aUIAln4zur4F7CdKAKyEi6NDzi')
   token_key = luigi.Parameter(default='FZbyPm1i3BMfiXKlKPuzBdRlvbenW09n8LX5OvgM85g')
@@ -81,7 +87,7 @@ class EnrichTrainerData(luigi.Task):
     input_file = self.date.strftime(self.input_prefix + '.' + '%Y%m%d')
     output_file = self.date.strftime(self.output_prefix + '.' + '%Y%m%d')
 
-    print(self.input_file())
+    print self.input_file()
     df = self.load_dataframe(self.input_file())
 
     self.output()[0].makedirs()
@@ -129,33 +135,37 @@ class EnrichTrainerData(luigi.Task):
         'tweet_trends_count',
         'tweet_symbols_count'])
       for tweet in data:
-        tweets_writer.writerow([
-          tweet['user']['id'],
-          tweet['user']['screen_name'],
-          tweet['user']['name'],
-          tweet['user']['verified'],
-          tweet['user']['friends_count'],
-          tweet['user']['followers_count'],
-          tweet['user']['listed_count'],
-          tweet['user']['statuses_count'],
-          tweet['user']['favourites_count'],
-          tweet['user']['description'],
-          tweet['user']['created_at'],
-          tweet['user']['location'] if tweet['user'].get('location') else 'null',
+        try:
+          tweets_writer.writerow([
+            tweet['user']['id'],
+            tweet['user']['screen_name'],
+            tweet['user']['name'],
+            tweet['user']['verified'],
+            tweet['user']['friends_count'],
+            tweet['user']['followers_count'],
+            tweet['user']['listed_count'],
+            tweet['user']['statuses_count'],
+            tweet['user']['favourites_count'],
+            tweet['user']['description'],
+            tweet['user']['created_at'],
+            tweet['user']['location'] if tweet['user'].get('location') else 'null',
 
-          tweet['id'],
-          tweet['created_at'],
-          re.findall('>(.*)<', tweet['source'])[0],
-          tweet['text'],
-          not tweet['retweeted'],
-          len(tweet['entities']['urls']),
-          len(tweet['entities']['user_mentions']),
-          len(tweet['entities']['hashtags']),
-          "",
-          len(tweet['entities']['symbols'])
-        ])
-        s += 1
-        if (s % 100 == 0): print('Written', s, 'rows to', file_name)
+            tweet['id'],
+            tweet['created_at'],
+            re.findall('>(.*)<', tweet['source'])[0],
+            tweet['text'],
+            not tweet['retweeted'],
+            len(tweet['entities']['urls']),
+            len(tweet['entities']['user_mentions']),
+            len(tweet['entities']['hashtags']),
+            "",
+            len(tweet['entities']['symbols'])
+          ])
+          s += 1
+          if (s % 100 == 0): print('Written', s, 'rows to', file_name)
+        except Exception, ex:
+          print ex
+
 
   def lookup_twitter_statuses(self, data):
     if len(data) == 0: return []
@@ -168,7 +178,7 @@ class EnrichTrainerData(luigi.Task):
     for x in range(0, max_mod):
       native_ids = data[(indices % max_mod) == x]['native_id']
       tweets += t.statuses.lookup(_id=','.join(native_ids), _timeout=3)
-      print('Downloaded tweet info for', len(tweets))
+      print 'Downloaded tweet info for', len(tweets)
       time.sleep(5)
 
     return tweets
@@ -176,13 +186,15 @@ class EnrichTrainerData(luigi.Task):
 class TrainRandomForestModel(luigi.Task):
   date         = luigi.Parameter(default=datetime.today())
   start        = luigi.Parameter(default=datetime(2015,07,01))
+  
   s3_csvs      = luigi.Parameter('s3://encorealert-luigi-development/actor_classification/csv/')
   s3_models    = luigi.Parameter('s3://encorealert-luigi-development/actor_classification/models/')
   
   local_csvs   = 'data/actor_classification/csv/'
-  local_model  = 'data/actor_classification/models/'
+  local_path  = 'data/actor_classification/models/'
 
-  tweets_features_cols = ['actor_favorites_count', 
+  tweets_features_cols = ['actor_summary',
+                          'actor_favorites_count', 
                           'actor_followers_count', 
                           'actor_friends_count', 
                           'actor_listed_count', 
@@ -197,7 +209,8 @@ class TrainRandomForestModel(luigi.Task):
                           'actor_registration_from_now'
                           ]
 
-  tweets_original_cols = ['actor_favorites_count', 
+  tweets_original_cols = ['actor_summary',
+                          'actor_favorites_count', 
                           'actor_followers_count', 
                           'actor_friends_count', 
                           'actor_listed_count', 
@@ -227,7 +240,10 @@ class TrainRandomForestModel(luigi.Task):
     yield RangeDailyBase(start=self.start, of='EnrichTrainerData')
 
   def output(self):
-    return S3Target(self.date.strftime(self.s3_models + '.' + '%Y%m%d'))
+    return {
+      'model': S3Target(self.model_path(self.s3_models)),
+      'counter': S3Target(self.counter_path(self.s3_models))
+      }
 
   def run(self):
     brands = self.concat_dataframes(self.local_csvs + '*.brand')
@@ -241,46 +257,53 @@ class TrainRandomForestModel(luigi.Task):
     del tweets['tweet_trends_count']
 
     tweets = self.generate_calculated_features(tweets)
+    tweets, counter = self.create_ngrams_with_bio(tweets)
 
-    cols = tweets[self.tweets_features_cols].columns
+    cols = tweets.columns
     cols = cols - ['class']
     print '### Train Columns:', cols
 
-    k_fold = KFold(n=len(tweets), n_folds=6, indices=False, shuffle=True)
-    b_scores, svc_scores = [], []
+    for fold in [6,2]:
+      k_fold = KFold(n=len(tweets), n_folds=fold, indices=False, shuffle=True)
+      b_scores, svc_scores = [], []
 
-    for train_indices, test_indices in k_fold:
-      train = np.asarray(tweets[train_indices][cols])
-      train_y    = np.asarray(tweets[train_indices]['class'])
+      for train_indices, test_indices in k_fold:
+        train = np.asarray(tweets[train_indices][cols])
+        train_y    = np.asarray(tweets[train_indices]['class'])
 
-      test = np.asarray(tweets[test_indices][cols])
-      test_y     = np.asarray(tweets[test_indices]['class'])
+        test = np.asarray(tweets[test_indices][cols])
+        test_y     = np.asarray(tweets[test_indices]['class'])
 
-      clf = RandomForestClassifier(n_estimators=25)
-      clf.fit(train, train_y)
-      clf_probs = clf.predict_proba(test)
-      print confusion_matrix(test_y, clf.predict(test))
-      print 'score:' + str(clf.score(test, test_y))
+        clf = RandomForestClassifier(n_estimators=25)
+        clf.fit(train, train_y)
+        clf_probs = clf.predict_proba(test)
+        print confusion_matrix(test_y, clf.predict(test))
+        print 'score:' + str(clf.score(test, test_y))
         
     forest = RandomForestClassifier(n_estimators=25)
     forest.fit(tweets[cols], tweets['class'])
 
-    score = forest.score(tweets[cols], tweets['class'])
-    print 'final score: ', score
+    if not os.path.exists(self.local_path):
+      os.makedirs(self.local_path)
 
-    if not os.path.exists(self.local_model):
-      os.makedirs(self.local_model)
+    joblib.dump(forest, self.model_path(self.local_path), compress=9)
+    joblib.dump(counter, self.counter_path(self.local_path), compress=9)
 
-    joblib.dump(forest, self.model_path(), compress=9)
+    with open(self.model_path(self.local_path)) as model_pickle:
+      with open(self.counter_path(self.local_path)) as counter_pickle:
+        with self.output()['model'].open(mode='w') as s3_model:
+          with self.output()['counter'].open(mode='w') as s3_counter:
+            s3_model.write(model_pickle.read())
+            s3_counter.write(counter_pickle.read())
 
-    with open(self.model_path()) as model:
-      with self.output().open(mode='w') as s3_model:
-          s3_model.write(model.read())
 
-    os.remove(self.model_path())
+    os.remove(self.model_path(self.local_path))
+    os.remove(self.counter_path(self.local_path))
 
-  def model_path(self):
-    return self.local_model + self.date.strftime('actor_classification_random_forest_%Y%m%d.pkl')
+  def model_path(self, directory):
+    return directory + self.date.strftime('actor_classification_random_forest_%Y%m%d.pkl')
+  def counter_path(self, directory):
+    return directory + self.date.strftime('bio_count_vectorizer_%Y%m%d.pkl')
     
   def actor_registration_from_now(self, registration):
     r = parser.parse(registration)
@@ -293,18 +316,47 @@ class TrainRandomForestModel(luigi.Task):
     int_cols = ['actor_followers_count', 'actor_friends_count', 'actor_favorites_count', 'actor_statuses_count', 'actor_listed_count', 'class']
     tweets[int_cols] = tweets[int_cols].astype(int)
 
-    tweets['manually_tweeting'] = tweets['tweet_generator'].apply(lambda entry: 1 if entry in self.manual_generators else 0)
-    tweets['actor_verified'] = tweets['actor_verified'].apply(lambda entry: 1 if entry else 0)
-    tweets['followers_friends_ratio'] = tweets['actor_followers_count']/tweets['actor_friends_count']
-    tweets['favourites_friends_ratio'] = tweets['actor_favorites_count']/tweets['actor_friends_count']
-    tweets['favourites_followers_ratio'] = tweets['actor_favorites_count']/tweets['actor_followers_count']
-    tweets['favourites_status_ratio'] = tweets['actor_favorites_count']/tweets['actor_statuses_count']
+    tweets.loc[:, ('manually_tweeting')] = tweets.loc[:, 'tweet_generator'].apply(lambda entry: 1 if entry in self.manual_generators else 0)
+    tweets.loc[:, ('actor_verified')] = tweets.loc[:, 'actor_verified'].apply(lambda entry: 1 if entry else 0)
+    tweets.loc[:, ('followers_friends_ratio')] = tweets.loc[:, 'actor_followers_count']/tweets.loc[:, 'actor_friends_count']
+    tweets.loc[:, ('favourites_friends_ratio')] = tweets.loc[:, 'actor_favorites_count']/tweets.loc[:, 'actor_friends_count']
+    tweets.loc[:, ('favourites_followers_ratio')] = tweets.loc[:, 'actor_favorites_count']/tweets.loc[:, 'actor_followers_count']
+    tweets.loc[:, ('favourites_status_ratio')] = tweets.loc[:, 'actor_favorites_count']/tweets.loc[:, 'actor_statuses_count']
 
-    tweets['actor_registration_from_now'] = tweets['actor_created_at'].apply(lambda registration: self.actor_registration_from_now(registration))
+    tweets.loc[:, ('actor_registration_from_now')] = tweets.loc[:, 'actor_created_at'].apply(lambda registration: self.actor_registration_from_now(registration))
 
-    tweets = tweets.replace([np.inf, -np.inf], 0).dropna(subset=self.tweets_features_cols)
+    tweets = tweets.loc[:, (self.tweets_features_cols)]
+    tweets = tweets.replace([np.inf, -np.inf], 0).dropna()
 
     return tweets
+
+  import nltk
+  from nltk import word_tokenize
+  from sklearn.feature_extraction.text import CountVectorizer
+
+  # #######
+  def tokenize(self, text):
+    tokenizer = nltk.RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(text)
+    tokens = [('NUM' if word.isdigit() else word) for word in tokens]
+    return tokens
+  # ######## 
+
+  def create_ngrams_with_bio(self, tweets):
+    bio_words_countvect = CountVectorizer(tokenizer=self.tokenize,
+                                                ngram_range=(1, 3), 
+                                                analyzer="word",
+                                                min_df = 30)
+
+    bio_word_matrix = bio_words_countvect.fit_transform(tweets['actor_summary'])
+    bio_word_matrix_df = pd.DataFrame(bio_word_matrix.A, columns=map(lambda name: 'bio_words_' + name, bio_words_countvect.get_feature_names()))
+    
+    del tweets['actor_summary']
+
+    # vocabulary = joblib.load('<my vectorizer file>') 
+    # vect = CountVectorizer(tokenizer=self.tokenize, ngram_range=(1,3), vocabulary = vocabulary)
+    return tweets.join(bio_word_matrix_df), bio_words_countvect.get_feature_names()
+
 
   def concat_dataframes(self, wildcard):
     files = glob.glob(wildcard)
@@ -319,20 +371,25 @@ class TrainRandomForestModel(luigi.Task):
 
 class DeployModel(luigi.Task):
   date = luigi.Parameter(default=datetime.today())
-  server = luigi.Parameter()
-  model_local_directory = luigi.Parameter(default='data/actor_classification/deploy/')
+  
+  model_local_directory = 'data/actor_classification/deploy/'
 
   def requires(self):
     return TrainRandomForestModel(self.date)
 
   def output(self):
-    return LocalTarget(self.model_path(self.model_local_directory))
+    for name in self.input():
+      s3_input = self.input()[name]
+      return LocalTarget(self.model_path(s3_input.path, self.model_local_directory))
 
   def run(self):
-    S3ToLocalTask(s3_path=self.input().path, local_path=self.model_path(self.model_local_directory)).run()
+    for name in self.input():
+      s3_input = self.input()[name]
+      S3ToLocalTask(s3_path=s3_input.path, local_path=self.model_path(s3_input.path, self.model_local_directory)).run()
 
-  def model_path(self, directory):
-    return directory + self.date.strftime('actor_classification_random_forest_%Y%m%d.pkl')
+  def model_path(self, path, directory):
+    filename = path.split('/')[-1]
+    return directory + filename
 
 if __name__ == "__main__":
     luigi.run()
