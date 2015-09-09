25bc223cc26defb7813511cf0fb56c8b5c1f3af0
Daniel Adornes <adornes@Daniels-MacBook-Pro.local>
2015-09-09 20:13:54 -0300

    Grouping by rule_id and filtering by segment

--- predictive_post.py
+++ predictive_post.py
@@ -5,7 +5,41 @@ import redis
 import mysql.connector
 
 from datetime import datetime
+from datetime import timedelta
 from luigi.contrib.mysqldb import MySqlTarget
+
+# Search for PredictivePostConfigurations and pass rule_id and segment as
+# parameter for the AggregateToRedis task
+class ConfigsToAggregation(luigi.Task):
+
+  db_host     = luigi.configuration.get_config().get('mysql', 'host', 'localhost')
+  db_port     = luigi.configuration.get_config().get('mysql', 'port', 3306)
+  db_database = luigi.configuration.get_config().get('mysql', 'database', 'encore_analytics_development')
+  db_user     = luigi.configuration.get_config().get('mysql', 'user', 'root')
+  db_password = luigi.configuration.get_config().get('mysql', 'password', '')
+
+  query =  "SELECT rule_id as rule_id, \
+              segment as segment \
+            FROM \
+              predictive_post_configurations" 
+
+  def requires(self):
+    connection = self.connect()
+    cursor = connection.cursor(buffered=True)
+    cursor.execute(self.query)
+
+    date = datetime.today() - timedelta(days=5)
+
+    return map(lambda rule_id, segment: AggregateToRedis(date=date, rule_id=rule_id, segment=segment), 
+               cursor)
+
+  def connect(self):
+    connection = mysql.connector.connect(user=self.db_user,
+                                         password=self.db_password,
+                                         host=self.db_host,
+                                         port=self.db_port,
+                                         database=self.db_database)
+    return connection
 
 class AggregateToRedis(luigi.Task):
 
@@ -19,7 +53,9 @@ class AggregateToRedis(luigi.Task):
   redis_port  = luigi.configuration.get_config().get('redis', 'port', 6379)
   redis_db    = luigi.configuration.get_config().get('redis', 'db', 0)
 
-  date = luigi.Parameter(default=datetime.today())
+  date    = luigi.DateParameter(default=datetime.today() - timedelta(days=5))
+  rule_id = luigi.Parameter()
+  segment = luigi.Parameter()
 
   query =  "SELECT MAX(engagement_score) as max_engagement, \
               AVG(engagement_score) as avg_engagement, \
@@ -30,18 +66,35 @@ class AggregateToRedis(luigi.Task):
               predictive_post_data \
             WHERE \
               created_at > DATE(%(date)s) \
+            AND \
+              rule_id = %(rule_id)d \
+            AND \
+              %(segment_clause)s \
             GROUP BY \
-              rule_id" 
+              rule_id "
+
+  def input(self):
+    return LocalTarget(self.input_file())
 
   def output(self):
-    return MySqlTarget(host=self.db_host, database=self.db_database, user=self.db_user, password=self.db_password, table='predictive_post', update_id=int(self.date.strftime("%Y%m%d%H")))
+    update_id = "{0}-{1}".format(self.date.strftime("%Y%m%d%H"), rule_id)
+    return MySqlTarget(host=self.db_host, 
+                       database=self.db_database, 
+                       user=self.db_user, 
+                       password=self.db_password, 
+                       table='predictive_post', 
+                       update_id=update_id)
 
   def run(self):
-    print ('ruunnnnnn!!')
+    print ('@@@ RUNNING AggregateToRedis')
     connection = self.connect()
     cursor = connection.cursor(buffered=True)
 
-    cursor.execute(self.query, {'date': self.date})
+    segment_clause = "segment = segment" if self.segment == None else "segment = '{0}'".format(self.segment)
+
+    cursor.execute(self.query, {'date':    self.date, 
+                                'rule_id': self.rule_id, 
+                                'segment_clause': segment_clause})
 
     redis_client = self.redis_client()
     for (_max, avg, std, count, rule) in cursor:
